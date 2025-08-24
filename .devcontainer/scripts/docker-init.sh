e0946210996eb109bdf9260f62a1290caf8594fb
Joshua Woolf <joshuawoolf87@gmail.com>
2025-08-24 05:37:42 +0200

    simpler

--- .devcontainer/scripts/docker-init.sh
+++ .devcontainer/scripts/docker-init.sh
@@ -2,80 +2,10 @@
 
 set -e
 
-function dockerd_start {
-    if [ -d /sys/kernel/security ] && ! mountpoint -q /sys/kernel/security; then
-        mount -t securityfs none /sys/kernel/security || {
-            echo >&2 'Could not mount /sys/kernel/security.'
-            echo >&2 'AppArmor detection and --privileged mode might break.'
-        }
-    fi
+mount -t securityfs none /sys/kernel/security
+mount -t tmpfs none /tmp
+mkdir -p /sys/fs/cgroup/init
 
-    # Mount /tmp (conditionally)
-    if ! mountpoint -q /tmp; then
-        mount -t tmpfs none /tmp
-    fi
+xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs
 
-    set_cgroup_nesting()
-    {
-        # cgroup v2: enable nesting
-        if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-            # move the processes from the root group to the /init group,
-            # otherwise writing subtree_control fails with EBUSY.
-            # An error during moving non-existent process (i.e., "cat") is ignored.
-            mkdir -p /sys/fs/cgroup/init
-            xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
-            # enable controllers
-            sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
-                > /sys/fs/cgroup/cgroup.subtree_control
-        fi
-    }
-
-    # Set cgroup nesting, retrying if necessary
-    retry_cgroup_nesting=0
-
-    until [ "${retry_cgroup_nesting}" -eq "5" ];
-    do
-        set +e
-            set_cgroup_nesting
-
-            if [ $? -ne 0 ]; then
-                echo "(*) cgroup v2: Failed to enable nesting, retrying..."
-            else
-                break
-            fi
-
-            retry_cgroup_nesting=`expr $retry_cgroup_nesting + 1`
-        set -e
-    done
-
-    ( dockerd > /tmp/dockerd.log 2>&1 ) &
-}
-
-retry_docker_start_count=0
-docker_ok="false"
-
-until [ "${docker_ok}" = "true"  ] || [ "${retry_docker_start_count}" -eq "5" ];
-do
-    dockerd_start
-
-    retry_count=0
-    until [ "${docker_ok}" = "true"  ] || [ "${retry_count}" -eq "5" ];
-    do
-        sleep 1s
-        set +e
-            docker info > /dev/null 2>&1 && docker_ok="true"
-        set -e
-
-        retry_count=`expr $retry_count + 1`
-    done
-
-    if [ "${docker_ok}" != "true" ] && [ "${retry_docker_start_count}" != "4" ]; then
-        echo "(*) Failed to start docker, retrying..."
-        set +e
-            sudo pkill dockerd
-            sudo pkill containerd
-        set -e
-    fi
-
-    retry_docker_start_count=`expr $retry_docker_start_count + 1`
-done
+( dockerd > /tmp/dockerd.log 2>&1 ) &
