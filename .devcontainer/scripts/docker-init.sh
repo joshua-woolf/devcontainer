517781b2be37a6792a97b403243f1bd69cf39e4d
Joshua Woolf <joshuawoolf87@gmail.com>
2025-08-24 07:12:16 +0200

    cleanup

--- .devcontainer/scripts/docker-init.sh
+++ .devcontainer/scripts/docker-init.sh
@@ -3,7 +3,6 @@
 set -e
 
 mount -t securityfs none /sys/kernel/security
-mount -t tmpfs none /tmp
 mkdir -p /sys/fs/cgroup/init
 
 xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs
